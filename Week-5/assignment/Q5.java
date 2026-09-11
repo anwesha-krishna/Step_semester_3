6e17ebf6398042d59d048c1e5cf7641356e60df6
Anwesha-29 <anwesha.k2906@gmail.com>
2026-09-11 13:28:02 +0530

    Add Week 5 Category C assignment problems (Q1-Q5)

---
+++ Week-5/assignment/Q5.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+class Player implements Comparable<Player> {
+    String name;
+    int matchesPlayed;
+    double battingAverage;
+    boolean injured;
+
+    public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
+        this.name = name;
+        this.matchesPlayed = matchesPlayed;
+        this.battingAverage = battingAverage;
+        this.injured = injured;
+    }
+
+    static boolean isDraftable(int matchesPlayed) {
+        return matchesPlayed >= 10;
+    }
+
+    static boolean isDraftable(int matchesPlayed, boolean injured) {
+        return matchesPlayed >= 5 && !injured;
+    }
+
+    @Override
+    public int compareTo(Player other) {
+        return Double.compare(other.battingAverage, this.battingAverage);
+    }
+
+    static String draftAndRank(Player[] players) {
+        int count = 0;
+        for (Player p : players) {
+            if (isDraftable(p.matchesPlayed) || isDraftable(p.matchesPlayed, p.injured)) {
+                count++;
+            }
+        }
+
+        Player[] draftable = new Player[count];
+        int index = 0;
+        for (Player p : players) {
+            if (isDraftable(p.matchesPlayed) || isDraftable(p.matchesPlayed, p.injured)) {
+                draftable[index++] = p;
+            }
+        }
+
+        Arrays.sort(draftable);
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < draftable.length; i++) {
+            result.append(i + 1).append(". ").append(draftable[i].name);
+            if (i != draftable.length - 1) {
+                result.append(" | ");
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+public class Q5 {
+    public static void main(String[] args) {
+        Player[] players = {
+                new Player("Virat", 15, 48.0, false),
+                new Player("Rahul", 7, 55.0, false),
+                new Player("Sameer", 3, 60.0, false),
+                new Player("Dev", 12, 20.0, true)
+        };
+        System.out.println(Player.draftAndRank(players));
+        // Expected: 1. Rahul | 2. Virat | 3. Dev
+    }
+}
